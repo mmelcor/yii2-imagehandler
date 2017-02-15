dd84f25f834bf44f4b186234b3efc795280ee7b6
Jeremiah Payne <jer@Jeremiahs-MacBook-Pro.example.com>
2017-02-15 09:30:29 -0700

    Added ability to use a different default language than English.

--- ImageHandler.php
+++ ImageHandler.php
@@ -24,7 +24,7 @@ class ImageHandler extends \yii\base\Component {
 		if(self::fileExists($path)) {
 			return $this->hostUrl.$this->siteFolder."/".$lang."/".$path;
 		} else {
-			return $this->hostUrl.$this->siteFolder."/en".$path;
+			return $this->hostUrl.$this->siteFolder."/".$defaultLang."/".$path;
 		}
 	}
 
