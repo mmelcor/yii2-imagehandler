1bb96675b74bf01c80f165728bfd9488d0ff416f
Jeremiah Payne <jer@Jeremiahs-MacBook-Pro.example.com>
2017-06-27 12:34:40 -0600

    Kevin fixed issues with the paths.

--- ImageHandler.php
+++ ImageHandler.php
@@ -11,46 +11,50 @@ use yii\helpers\Html;
  * @package mmelcor\imagehandler
  */
 class ImageHandler extends \yii\base\Component {
-	public $hostUrl;
-	public $siteFolder;
-	public $defaultLang;
 
-	/**
-	 * Calls fileExists and returns an image path if true.
-	 * @return string
-	 */
-	public function getImage($path) {
-		$lang = Yii::$app->language;
-		if(self::fileExists($path)) {
-			return $this->hostUrl."/".$this->siteFolder."/".$lang.$path;
-		} else {
-			return $this->hostUrl."/".$this->siteFolder."/".$this->defaultLang.$path;
-		}
-	}
+    public $hostUrl;
+    public $siteFolder;
+    public $defaultLang;
 
-	/**
-	 * Checks if the requested image exists and returns true or false.
-	 * @return boolean
-	 */
-	public function fileExists($path) {
-		$ch = curl_init($this->hostUrl.$this->siteFolder."/".$path);
+    /**
+     * Calls fileExists and returns an image path if true.
+     * @return string
+     */
+    public function getImage($path) {
 
-		curl_setopt($ch, CURLOPT_NOBODY, true);
+        $lang = Yii::$app->language;
+        $test = '/'. $lang . $path;
 
-		$result = curl_exec($ch);
+        if(self::fileExists($test)) {
+        return $this->hostUrl . "/" .$this->siteFolder . $test;
+        } else {
+        return $this->hostUrl . "/" . $this->siteFolder . "/" .$this->defaultLang . $path;
+        }
+    }
 
-		$ret = false;
+    /**
+     * Checks if the requested image exists and returns true or false.
+     * @return boolean
+     */
+    public function fileExists($path) {
 
-		if($result !== false) {
-			$status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $ch = curl_init($this->hostUrl . "/" . $this->siteFolder . $path);
 
-			if($status == 200) {
-				$ret = true;
-			}
-		}
+        curl_setopt($ch, CURLOPT_NOBODY, true);
 
-		curl_close($ch);
+        $result = curl_exec($ch);
+        $ret = false;
 
-		return $ret;
-	}
+        if($result !== false) {
+        $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        if($status == 200) {
+            $ret = true;
+        }
+        }
+
+        curl_close($ch);
+
+        return $ret;
+    }
 }
